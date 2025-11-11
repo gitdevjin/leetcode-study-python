bd908b5fc2430de4285465256e020030ff1fb495
gitdevjin <dev.hjshin@gmail.com>
2025-11-11 12:08:21 -0500

    problem 1 and 2 solved

---
+++ 2_add_two_numbers.py
@@ -0,0 +1,101 @@
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+# Example 1:
+
+
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+# Example 2:
+
+# Input: l1 = [0], l2 = [0]
+# Output: [0]
+# Example 3:
+
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# Output: [8,9,9,9,0,0,0,1]
+ 
+
+# Constraints:
+
+# The number of nodes in each linked list is in the range [1, 100].
+# 0 <= Node.val <= 9
+# It is guaranteed that the list represents a number that does not have leading zeros.
+
+
+################################### My Answer ########################################
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+
+        head = ListNode()
+        current = head
+
+        left = l1
+        right = l2
+        excess = 0
+
+        while current:
+            current.val += excess
+
+            if left is not None:
+                current.val += left.val
+                left = left.next
+
+            if right is not None:
+                current.val += + right.val
+                right = right.next
+
+            if current.val >= 10:
+                excess = 1
+                current.val = current.val % 10
+            else:
+                excess = 0
+                
+            if left is not None or right is not None or excess > 0:
+                current.next = ListNode()
+                current = current.next
+                current.val = 0
+            else:
+                current = None
+
+        return head
+
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
+
+#################################### ChatGPT Answer ##########################################
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode()   # dummy head
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            sum = carry
+
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+
+            carry = sum // 10
+            current.next = ListNode(sum % 10)
+            current = current.next
+
+        return dummy.next
