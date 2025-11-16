079a014eaf2bd59b1c3cc6768daac06109bfaae8
gitdevjin <dev.hjshin@gmail.com>
2025-11-16 16:09:53 -0500

    GPT Answer Added For study

--- 5_longest_palindromic.py
+++ 5_longest_palindromic.py
@@ -52,3 +52,34 @@ class Solution(object):
 sol = Solution()
 answer = sol.longestPalindrome("abbc")
 print("answer: " + answer)
+
+
+######################## ChatGPT Answer #############################
+### Read and Absorb
+class Solution(object):
+    def longestPalindrome(self, s):
+        if len(s) <= 1:
+            return s
+
+        start, end = 0, 0  # indices of longest palindrome found
+
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            # return the length of palindrome
+            return left + 1, right - 1
+
+        for i in range(len(s)):
+            # Odd length palindrome
+            l1, r1 = expand(i, i)
+            # Even length palindrome
+            l2, r2 = expand(i, i + 1)
+
+            # Choose the longer one
+            if r1 - l1 > end - start:
+                start, end = l1, r1
+            if r2 - l2 > end - start:
+                start, end = l2, r2
+
+        return s[start:end + 1]
